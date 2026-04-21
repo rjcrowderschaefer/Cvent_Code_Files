3ff30e10073ab0cd656227e6796304854f18383f
RJ Crowder-Schaefer <51172000+rjcrowderschaefer@users.noreply.github.com>
2026-04-21 13:26:01 -0400

    Delete Cvent Code Files/Cvent Widgets/cvent-widget-demo directory

--- Cvent Code Files/Cvent Widgets/cvent-widget-demo/editor.js
+++
@@ -1,9 +0,0 @@
-CventCustomWidget.registerEditor("valid-widget", {
-    render(container, config) {
-      container.innerHTML = "<p>This widget has no configurable options.</p>";
-    },
-    save() {
-      return {};
-    }
-  });
-  
